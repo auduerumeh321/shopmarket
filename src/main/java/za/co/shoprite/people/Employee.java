74d97ae7496e053064b13e32a7b9c8c6b430a87a
auduerumeh321 <erumehjonathan@yahoo.com>
2020-08-22 10:08:50 +0200

    first commit to shopmarket

---
+++ src/main/java/za/co/shoprite/people/Employee.java
@@ -0,0 +1,27 @@
+package za.co.shoprite.people;
+
+import za.co.shopprite.person.;
+
+
+public  Employee extend Person{
+private long phoneNumber;
+private Scanner scan;
+
+
+
+
+public void setPhonenumber(long phoneNumber){
+this.phoneNumber=phoneNumber;
+}
+public int getPhonenumber(){
+return phoneNumber;
+}
+
+
+public void displayCash(){
+retun "how much is your cash"; 
+
+
+}
+
+}
---
+++ src/main/java/za/co/shoprite/transaction/Payment.java
@@ -0,0 +1,23 @@
+package za.co.shoprite.transcation.Payment;
+
+public class Payment extend Person{
+
+private String name;
+
+private Scanner scan;
+
+public void setName;
+this.name=name;
+
+}
+
+public String getName(){
+return name;
+
+}
+
+void displaycash(){
+System.out.print(how mmuch is your cash);
+
+
+}
